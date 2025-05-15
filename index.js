63344b643b90f13dac6d1870414c9149e4a80b7d
agusperez11 <perezagustinadrian@gmail.com>
2025-05-15 18:10:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
+
 const antlr4 = require('antlr4');
 const fs = require('fs');
-const SubCLiteLexer = require('./output/SubCLiteLexer').SubCLiteLexer;
-const SubCLiteParser = require('./output/SubCLiteParser').SubCLiteParser;
-const SubCLiteListener = require('./output/SubCLiteListener').SubCLiteListener;
-const SubCLiteVisitor = require('./output/SubCLiteVisitor').SubCLiteVisitor;
-
-// Cargar el archivo de entrada
+const SubCLiteLexer = require('./antlr/SubCLiteLexer').SubCLiteLexer;
+const SubCLiteParser = require('./antlr/SubCLiteParser').SubCLiteParser;
+const SubCLiteListener = require('./antlr/SubCLiteListener').SubCLiteListener;
+const SubCLiteVisitor = require('./antlr/SubCLiteVisitor').SubCLiteVisitor;
 const input = fs.readFileSync('input.txt', 'utf8');
 const chars = new antlr4.InputStream(input);
 const lexer = new SubCLiteLexer(chars);
 const tokens = new antlr4.CommonTokenStream(lexer);
-
-// Mostrar tabla de tokens
-tokens.fill();
-fs.writeFileSync('output/tokens.txt', tokens.tokens.map(tok => 
-  `Token(${lexer.symbolicNames[tok.type]}): "${tok.text}" (line ${tok.line})`
-).join('\n'));
-
-// Parser
 const parser = new SubCLiteParser(tokens);
-parser.buildParseTrees = true;
-
-try {
-    const tree = parser.programa(); // Punto de entrada
-    const treeStr = tree.toStringTree(parser.ruleNames);
-    fs.writeFileSync('output/parseTree.txt', treeStr);
-    console.log("✔️ Análisis sintáctico exitoso.");
-} catch (e) {
-    console.error("❌ Error de sintaxis:", e.message);
+class ErrorListener extends antlr4.error.ErrorListener {
+    syntaxError(recognizer, offendingSymbol, line, column, msg) {
+        console.error(`Error de sintaxis en línea ${line}, columna ${column} : ${msg}`);
+        ProcessingInstruction.exit(1);
+    }
 }
+parser.removeErrorListeners();
+parser.addErrorListener(new ErrorListener()); 
+const tree = parser.programa();
+console.log('\nEntrada valida. anslisis completado sin errores.');
+console.log('\nTabla de lexemas y tokens:');
+tokens.fill();
+tokens.getTokens().forEach(token => {
+    const type = lexer.symbolicNames[token.type];
+    if (type) {
+        console.log (`Lexema: '${token.text}' => Token: ${type}´ `); 
+        }
+    });
+const {Trees} = import('antlr4/tree/Tree');
+console.log('\nArbol de sintactico:');
+console.log(Trees.toStringTree(tree, parser.ruleNames));
